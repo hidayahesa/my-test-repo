6917e38871d8844c04be11e5bc09665ee132527e
hidayahesa <noorhidayah.esa95@yahoo.com>
2021-08-24 14:54:04 +0800

    Finished working with API

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+// We want to talk to an API:
+// What is our endpoint?
+let url = `https://restcountries.eu/rest/v2/name/Malaysia`; // assume it s a GET
+
+function waitForData(res) {
+  return res.json();
+}
+
+function handleData(data) {
+  let pop = data[0].population;
+  console.log(`The population of Malaysia is ${pop}`);
+}
+
+fetch(url, { method: "GET" }).then(waitForData).then(handleData);
+
+// What parameters are required?
+// What parameters are optional?
+// What is HTTP method?
